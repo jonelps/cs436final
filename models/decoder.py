5bb17a65d5538cabd7ba6fbdb4052f78c54e5125
jonelps <105315585+jonelps@users.noreply.github.com>
2024-11-16 22:56:03 -0500

    Update decoder.py with nonautoregress decoder for testing

--- models/decoder.py
+++ models/decoder.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+'''
 class DecoderLayer(nn.Module):
     def __init__(self, self_attention, cross_attention, d_model, d_ff=None,
                  dropout=0.1, activation="relu"):
@@ -44,6 +45,46 @@ class Decoder(nn.Module):
     def forward(self, x, cross, x_mask=None, cross_mask=None):
         for layer in self.layers:
             x = layer(x, cross, x_mask=x_mask, cross_mask=cross_mask)
+
+        if self.norm is not None:
+            x = self.norm(x)
+
+        return x
+    '''
+
+class ParallelDecoderLayer(nn.Module):
+    def __init__(self, cross_attention, d_model, d_ff=None, dropout=0.1, activation="relu"):
+        super(ParallelDecoderLayer, self).__init__()
+        d_ff = d_ff or 4 * d_model
+        self.cross_attention = cross_attention
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.activation = F.relu if activation == "relu" else F.gelu
+
+    def forward(self, x, cross, cross_mask=None):
+        # Cross-attention without autoregressive constraints
+        x = x + self.dropout(self.cross_attention(
+            x, cross, cross, attn_mask=cross_mask
+        )[0])
+        x = self.norm1(x)
+
+        # Feed-forward layer
+        y = self.dropout(self.activation(self.conv1(x.transpose(-1, 1))))
+        y = self.dropout(self.conv2(y).transpose(-1, 1))
+        return self.norm2(x + y)
+
+class ParallelDecoder(nn.Module):
+    def __init__(self, layers, norm_layer=None):
+        super(ParallelDecoder, self).__init__()
+        self.layers = nn.ModuleList(layers)
+        self.norm = norm_layer
+
+    def forward(self, x, cross, cross_mask=None):
+        for layer in self.layers:
+            x = layer(x, cross, cross_mask=cross_mask)
 
         if self.norm is not None:
             x = self.norm(x)
